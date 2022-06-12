7817a72af5b738b6616c4046b2f9b5fdc9e0551a
Blas Cenzano Dragun <blascd@gmail.com>
2022-06-12 13:58:04 -0300

    Quedo sin subir la clase que maneja las excepciones para enviar el mensaje de error al usuario

---
+++ src/main/java/com/bd/tpfinal/utils/ControllerExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.bd.tpfinal.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(DeliveryException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(HttpServletRequest request, Exception ex) {
+        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
